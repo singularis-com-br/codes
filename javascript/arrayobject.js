b34ece00dc4a32e7de09acf22dbd06c0b774609b
singularis-com-br <98231327+singularis-com-br@users.noreply.github.com>
2022-12-09 17:59:58 -0300

    Create arrayobject.js

---
+++ javascript/arrayobject.js
@@ -0,0 +1,68 @@
+/*
+ESTE COMPONENTE SERVE PARA PEGAR TODOS OS COMPONENTES DO FORMULARIO MESMO Q ELES ESTEJAM EM FORMATO DE ARRAY, EX: name="componente[1]['teste']"
+*/
+$.fn.serializeObject = function(el){
+
+var self = this,
+		json = {},
+		push_counters = {},
+		patterns = {
+				"validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
+				"key":      /[a-zA-Z0-9_]+|(?=\[\])/g,
+				"push":     /^$/,
+				"fixed":    /^\d+$/,
+				"named":    /^[a-zA-Z0-9_]+$/
+		};
+
+
+this.build = function(base, key, value){
+		base[key] = value;
+		return base;
+};
+
+this.push_counter = function(key){
+		if(push_counters[key] === undefined){
+				push_counters[key] = 0;
+		}
+		return push_counters[key]++;
+};
+ 
+//$.each($(this).serializeArray(), function(){
+$.each($(this).serializeArray(), function(){
+
+		// Skip invalid keys
+		if(!patterns.validate.test(this.name)){
+				return;
+		}
+
+		var k,
+				keys = this.name.match(patterns.key),
+				merge = this.value,
+				reverse_key = this.name;
+
+		while((k = keys.pop()) !== undefined){
+				// Adjust reverse_key
+				reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
+				// Push
+				if(k.match(patterns.push)){
+						merge = self.build([], self.push_counter(reverse_key), merge);
+				}
+				// Fixed
+				else if(k.match(patterns.fixed)){
+						merge = self.build([], k, merge);
+				}
+				// Named
+				else if(k.match(patterns.named)){
+						merge = self.build({}, k, merge);
+				}
+		}
+
+		json = $.extend(true, json, merge);
+});
+
+return json;
+}; 
+
+
+
+var  ConvertedJSON = $(this).closest("td").find(".form-control").serializeObject();
